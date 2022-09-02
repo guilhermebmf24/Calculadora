9f03c700b6dcf67be3bf02d53ab7013ce1fe7968
guilhermebmf24 <guilhermebmf24@gmail.com>
2022-09-01 21:10:41 -0300

    criação da calculadora
    criação da calculadora, inicio do código JS,  alterações no CSS, e criação de classes pros botões

---
+++ main.js
@@ -0,0 +1,15 @@
+const lista_de_botoes = document.querySelectorAll('input')
+
+const tela = document.querySelector('#resultado')
+
+function funcao_da_tecla(){
+    console.log("funfou")
+}
+
+
+/*
+for (let i = 0; i <= lista_de_botoes.length; i++){
+    lista_de_botoes[i].addEventListener('mousedown', ()=>{
+    }
+}
+*/
